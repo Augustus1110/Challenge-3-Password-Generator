d95e2be81dab0e14c1d1748a477c8fefc6df49e9
Augustus1110 <donahi22@gmail.com>
2023-05-12 23:13:20 -0700

    Added characters as variables, created generator function

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-// Assignment Code
+//This is the event listener for the red button that was present in the code downloaded from the class repo 
+
 var generateBtn = document.querySelector("#generate");
 
-var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+//Defined characters that will be used to generate password
+
+var lowercaseLetters = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+
+var uppercaseLetters = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+
+var numbers = ('1', '2', '3', '4' '5', '6', '7', '8', '9', '0');
+
+var specialCharacters = ('!@#$%^&*()_+=');
 
 
-var numbers = ['1', '2', '3', '4' '5'];
-
-function generatePassword () {
-  let index = 0;
-  var input = prompt ("Input text here")
+//Created function to generate password
+function generatePassword(passwordLength) {
+  var allCharacters = lowercaseLetters + uppercaseLetters + numbers + specialCharacters;
+  var password = '';
+  
+  for (var i = 0; i < passwordLength; i++) {
+      var randomIndex = Math.floor(Math.random() * allCharacters.length);
+      password += allCharacters[randomIndex];
+  }
+  
+  return password;
 }
 
-while (true) {
-  yi
-}
+//This calls the function to generate the password 
 
-function generatePassword() {
-  var generated = "password";
-  var temp = Math.random();
-  var generatedIndex = Math.floor (Math.random()*generated.length);
-  generated=generated [generatedIndex];
-  //return generated;
-  //return password;
-
-}
+var passwordLength = ();  // Choose your password length
+var password = generatePassword(passwordLength);
+console.log(password);
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(passwordLength);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -37,3 +44,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
