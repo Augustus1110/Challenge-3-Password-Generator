b7a7d464e6386c2f13a0da543e208a297def43a6
Augustus1110 <donahi22@gmail.com>
2023-05-13 21:20:48 -0700

    defined user choosing variables

--- script.js
+++ script.js
@@ -4,26 +4,42 @@ var generateBtn = document.querySelector("#generate");
 
 //Defined characters that will be used to generate password
 
-var lowercaseLetters = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+var lowercaseLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-var uppercaseLetters = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+var uppercaseLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-var numbers = ('1', '2', '3', '4' '5', '6', '7', '8', '9', '0');
+var numbers = ['1', '2', '3', '4' '5', '6', '7', '8', '9', '0'];
 
-var specialCharacters = ('!@#$%^&*()_+=');
+var specialCharacters = '~`!@#$%^&*()_+={}[]|\\'.split('');
 
 
 //Created function to generate password
+//password length is a string
 function generatePassword(passwordLength) {
-  var allCharacters = lowercaseLetters + uppercaseLetters + numbers + specialCharacters;
-  var password = '';
+  var passwordLength = prompt ('How long would you like your password to be? Choose from 8 to 128 characters');
   
+
+  if (parseInt(passwordLength) < 8 || parseInt(passwordLength) > 128) {
+    alert('You must type a number from 8 to 128.');
+    return null;
+  }
+  //alert will create a pop up alerting user about something, confirm is a true/false boolean, prompt returns a prompt for user 
+  
+  var choseLowercase = confirm('Would you like to use lowercase letters in your password?');
+  var choseUppercase = confirm('Would you like to use uppercase letters in your password?');
+  var choseNumbers = confirm('Would you like to use numbers in your password?');
+  var choseSpecialCharacters = confirm('Would you like to use special characters in your password?')
+
+  //each true response will redefine the value of 
+  var allCharacters = [];
+  
+  var password ='';
   for (var i = 0; i < passwordLength; i++) {
       var randomIndex = Math.floor(Math.random() * allCharacters.length);
       password += allCharacters[randomIndex];
   }
   
-  return password;
+  return writePassword(password);
 }
 
 //This calls the function to generate the password 
@@ -35,7 +51,7 @@ console.log(password);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(passwordLength);
+  var passwordText = generatePassword(passwordLength);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
